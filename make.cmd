1ef7fb1e106ca75a44611aaa9c57581d4baa17a6
prokushev <yuri.prokushev@gmail.com>
2024-02-19 11:10:53 +0700

    cleanup

--- make.cmd
+++ make.cmd
@@ -1,33 +1,35 @@
+@echo off
+
 SET PATH=tools;%PATH%
 
-asw -lU -i . src\term\cpm64-term.asm > cpm64-term.lst
-p2bin src\term\cpm64-term.p bin\cpm64-term.bin
+asw -LU -i . src\term\term.asm -olist log\term.lst -o obj\term.p
+p2bin obj\term.p bin\term.bin
 
-asw -lU -i . src\bios\cpm64-bios.asm > cpm64-bios.lst
-p2bin src\bios\cpm64-bios.p bin\cpm64-bios.bin
+asw -LU -i . src\bios\bios.asm -olist log\bios.lst -o obj\bios.p
+p2bin obj\bios.p bin\bios.bin
 
-asw -lU -i . src\bdos\cpm64-bdos.asm > cpm64-bdos.lst
-p2bin src\bdos\cpm64-bdos.p bin\cpm64-bdos.bin
+asw -LU -i . src\bdos\bdos.asm -olist log\bdos.lst -o obj\bdos.p
+p2bin obj\bdos.p bin\bdos.bin
 
-asw -lU -i . src\ccp\cpm64-ccp.asm > cpm64-ccp.lst
-p2bin src\ccp\cpm64-ccp.p bin\cpm64-ccp.bin
+asw -LU -i . src\ccp\ccp.asm -olist log\ccp.lst -o obj\ccp.p
+p2bin obj\ccp.p bin\ccp.bin
 
-asw -lU -i . src\ch\ch.asm > ch.lst
-p2bin src\ch\ch.p com\ch.com
+asw -LU -i . src\ch\ch.asm -olist log\ch.lst -o obj\ch.p
+p2bin obj\ch.p com\ch.com
 
-asw -lU -i . -i bin -i com src\loader\cpm64-loader.asm > cpm64-loader.lst
-p2bin src\loader\cpm64-loader.p bin\cpm64-loader.bin
+asw -LU -i . -i bin -i com src\loader\loader.asm -olist log\loader.lst -o obj\loader.p
+p2bin obj\loader.p bin\loader.bin
 
-asw -lU src\chdisk\chdisk.asm > chdisk.lst
-p2bin src\chdisk\chdisk.p com\chdisk.com
+asw -LU src\chdisk\chdisk.asm -olist log\chdisk.lst -o obj\chdisk.p
+p2bin obj\chdisk.p com\chdisk.com
 
-asw -lU src\dump\dump.asm > dump.lst
-p2bin src\dump\dump.p com\dump.com
+asw -LU src\dump\dump.asm -olist log\dump.lst -o obj\dump.p
+p2bin obj\dump.p com\dump.com
 
-asw -lU src\romdisk\romdisk.asm > romdisk.lst
-p2bin src\romdisk\romdisk.p com\romdisk.com
+asw -LU src\romdisk\romdisk.asm -olist log\romdisk.lst -o obj\romdisk.p
+p2bin obj\romdisk.p com\romdisk.com
 
-bin2rk bin\cpm64-loader.bin 12544
+bin2rk bin\loader.bin 12544
 bin2rk com\ch.com 256
 
 :asw -l comheader.asm > comheader.lst
